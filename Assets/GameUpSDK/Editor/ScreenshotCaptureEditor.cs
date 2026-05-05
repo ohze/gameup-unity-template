7d07beff96ef4574155e89c85689a1776e8b2001
DuyOhze119 <duylv@ohzegames.com>
2026-05-05 13:43:53 +0700

    Move ScreenshotCapture and update namespaces
    Move ScreenshotCapture.cs into Scripts/Runtime and rename its .meta accordingly. Update namespaces from GameUpSDK to GameUp.SDK (including the editor class namespace) to match the new project structure and naming convention.

--- Assets/GameUpSDK/Editor/ScreenshotCaptureEditor.cs
+++ Assets/GameUpSDK/Editor/ScreenshotCaptureEditor.cs
@@ -1,7 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace GameUpSDK.Editor
+namespace GameUp.SDK.Editor
 {
     [CustomEditor(typeof(ScreenshotCapture))]
     public class ScreenshotCaptureEditor : UnityEditor.Editor
