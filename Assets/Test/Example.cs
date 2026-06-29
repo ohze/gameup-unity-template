2045532165e150cf434d5fd820b8fd7706285532
DuyLVGameUp <duylv@ohzegames.com>
2026-06-29 22:54:35 +1400

    update logic ChangeSpriteResizeTweenNav, fix bug

--- Assets/Test/Example.cs
+++ Assets/Test/Example.cs
@@ -1,63 +1,65 @@
 using UnityEngine;
-using GameUp.Core;
-using GameUp.Core.UI;
-
-public class Example : MonoBehaviour
+namespace Test
 {
-    public PlayerGameMainData playerGameMainData;
-    private void Start()
-    {
-        playerGameMainData = PlayerGameMainData.Create();
-        TimeUtils.Initialize();
-    }
 
-    [Button]
-    public void LevelUp()
-    {
-        playerGameMainData.LevelUp();
-    }
 
-    [Button]
-    public void TestAudio()
+    public class Example : MonoBehaviour
     {
-        //AudioManager.PlayAudio(AudioID.Hit_Death);
-    }
+        // public PlayerGameMainData playerGameMainData;
+        // private void Start()
+        // {
+        //     //playerGameMainData = PlayerGameMainData.Create();
+        //     TimeUtils.Initialize();
+        // }
 
-    [Button]
-    public void ShowPopup()
-    {
-        PopupTest.OpenViewAsync();
-    }
+        // [Button]
+        // public void LevelUp()
+        // {
+        //     //playerGameMainData.LevelUp();
+        // }
 
-    [Button]
-    public void ClosePopup()
-    {
-        PopupTest.CloseView();
-    }
+        // [Button]
+        // public void TestAudio()
+        // {
+        //     //AudioManager.PlayAudio(AudioID.Hit_Death);
+        // }
 
-    [Button]
-    public void ShowScreen()
-    {
-        ScreenTest.OpenViewAsync();
-    }
+        // [Button]
+        // public void ShowPopup()
+        // {
+        //     PopupTest.OpenViewAsync();
+        // }
 
-    [Button]
-    public void ShowScreen2()
-    {
-        ScreenTest2.OpenViewAsync();
-    }
+        // [Button]
+        // public void ClosePopup()
+        // {
+        //     PopupTest.CloseView();
+        // }
 
-    [Button]
-    public void Preload()
-    {
-        UIPopup.PreloadPopupByTypesAsync(typeof(PopupTest));
-        UIScreen.PreloadViewByTypeAsync(typeof(ScreenTest));
-    }
+        // [Button]
+        // public void ShowScreen()
+        // {
+        //     ScreenTest.OpenViewAsync();
+        // }
 
-    [Button]
-    public void Preload2()
-    {
-        UIPopup.PreloadPopupByTypesAsync(typeof(PopupTest));
-        UIScreen.PreloadViewByTypesAsync(typeof(ScreenTest), typeof(ScreenTest2));
+        // [Button]
+        // public void ShowScreen2()
+        // {
+        //     ScreenTest2.OpenViewAsync();
+        // }
+
+        // [Button]
+        // public void Preload()
+        // {
+        //     UIPopup.PreloadPopupByTypesAsync(typeof(PopupTest));
+        //     UIScreen.PreloadViewByTypeAsync(typeof(ScreenTest));
+        // }
+
+        // [Button]
+        // public void Preload2()
+        // {
+        //     UIPopup.PreloadPopupByTypesAsync(typeof(PopupTest));
+        //     UIScreen.PreloadViewByTypesAsync(typeof(ScreenTest), typeof(ScreenTest2));
+        // }
     }
 }
